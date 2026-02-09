94da7572495af8fd4e9caef333a29be5c9237cc2
Lauri <labe2301@student.miun.se>
2026-02-09 22:48:47 +0100

    Create Event-interface

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,16 +3,13 @@ export interface User {
     username: string;
     password: string;
     isAdmin: boolean;
-
     firstName: string;
     surname: string;
     email: string;
     phone: string;
     location: string;
-
     gender: 'man' | 'woman';
     age: number;
-
     interests: number[]; // interestsID's
     matches: number[]; // userID's
     registeredEvents: number[]; // eventID's
