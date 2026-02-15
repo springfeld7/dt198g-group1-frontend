5fec2dbe8c8cd2234358604ab96c5da48f475cf1
Lauri <labe2301@student.miun.se>
2026-02-15 02:42:24 +0100

    Implement signup

--- src/app/services/backend-service.service.ts
+++ src/app/services/backend-service.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { Interest } from '../models/interest';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BackendServiceService {
+  private API_URL: string = 'http://localhost:3000/api/v1';
 
-  constructor() { }
+  constructor() {}
+
+  async getAllInterests(): Promise<any> {
+    try {
+      const response = await fetch(`${this.API_URL}/interests`, {
+        method: 'GET',
+        credentials: 'include',
+      });
+      
+      if (response.ok) {
+        return await response.json();
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+    } catch (error: any) {
+      throw error;
+    }
+  }
+
+  async registerUser(user: any): Promise<any> {
+    try {
+      // Send POST request to backend to log the user in
+      const response = await fetch(`${this.API_URL}/auth/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+        credentials: "include"
+      });
+
+      if (!response.ok) {
+        const errorMsg = await response.json();
+        
+        throw new Error(errorMsg.error);
+      }
+      
+      const createdUser = await response.json();
+
+      return createdUser;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
 }
