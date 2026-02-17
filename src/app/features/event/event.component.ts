7fb265a4f4a022f8d5cfca9faae07c2c67d40202
thsp1900 <thsp1900@student.miun.se>
2026-02-17 09:42:13 +0100

    Add event component

--- src/app/features/event/event.component.ts
+++ src/app/features/event/event.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Event } from '../../models/event';
 
 @Component({
   selector: 'app-event',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss'
 })
 export class EventComponent {
 
+  @Input() event!: Event;
+  @Input() isRegistered: boolean = false;
+  @Input() userGender: 'man' | 'woman' | null = null;
+
+  @Output() registerClicked = new EventEmitter<void>();
+  @Output() unregisterClicked = new EventEmitter<void>();
+
+  /** Max spots per gender */
+  get maxPerGender(): number {
+    return this.event.maxSpots / 2;
+  }
+
+  /** Number of registered women */
+  get registeredWomenCount(): number {
+    return this.event.registeredWomen.length;
+  }
+
+  /** Number of registered men */
+  get registeredMenCount(): number {
+    return this.event.registeredMen.length;
+  }
+
+  /** Emits an event when the user clicks the register button. */
+  onRegister(): void {
+    this.registerClicked.emit();
+  }
+
+  /** Emits an event when the user clicks the unregister button. */
+  onUnregister(): void {
+    this.unregisterClicked.emit();
+  }
 }
