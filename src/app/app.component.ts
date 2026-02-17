f3c2944f76029ff460590804cd84863f251981b4
Loviza Sahlén <losa2300@student.miun.se>
2026-02-17 13:38:17 +0100

    Fix: Import MessageComponent in AppComponent & CommonModule in MessageComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { MessageComponent } from "./features/message/message.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MessageComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
--- src/app/features/message/message.component.ts
+++ src/app/features/message/message.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MessageService } from '../../services/message.service';
+import { CommonModule } from '@angular/common';
 
 
 /**
@@ -17,6 +18,7 @@ import { MessageService } from '../../services/message.service';
  */
 @Component({
   selector: 'app-message',
+  imports: [CommonModule],
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.scss']
 })
