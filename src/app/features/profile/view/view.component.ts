ccbecdba45c189c692fcbbcffd647ed4b3589dd9
Loviza Sahlén <losa2300@student.miun.se>
2026-02-23 17:49:10 +0100

    Add placeholder picture and labels for info

--- src/app/features/profile/view/view.component.ts
+++ src/app/features/profile/view/view.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BackendService } from '../../../services/backend.service';
+import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './view.component.html',
   styleUrl: './view.component.scss'
 })
 export class ViewComponent {
+  private backendService = inject(BackendService);
+  private authService = inject(AuthService);
+  isLoggedIn: boolean = false;
+  imageSrc: string = '';
+  placeholderSrc: string = '/profile.svg';
+  isLoaded: boolean = false;
 
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn$().value;
+  }
 }
