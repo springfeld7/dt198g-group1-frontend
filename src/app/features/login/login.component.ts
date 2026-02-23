56062937b1e0db2d3df211cfbdc8541712f3fe52
thsp1900 <thsp1900@student.miun.se>
2026-02-23 18:43:20 +0100

    Refactor login component to integrate message service for error handling

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { MessageService } from '../../services/message.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,21 +12,23 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   private authService = inject(AuthService);
+  private messageService = inject(MessageService);
   private router = inject(Router);
 
   username: string = "";
   password: string = "";
-  errorMessage: string = "";
 
-  submit(): void {
-    this.authService.login(this.username, this.password).then(
-      (_) => {
-        this.errorMessage = "";
-        this.router.navigate(["/"]);
-      },
-      (error) => {
-        this.errorMessage = error.error.error;
-      }
-    )
+  /**
+   * Handles the login form submission.
+   */
+  async submit(): Promise<void> {
+    try {
+      //this.messageService.clearMessage();
+      await this.authService.login(this.username, this.password);
+      this.router.navigate(["/"]);
+
+    } catch (err: any) {
+      this.messageService.showErrorMessage(err.message, 5);
+    }
   }
 }
