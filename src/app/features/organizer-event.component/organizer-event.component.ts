5086b66e9e0224ac172af98dea359d3cd7407c6e
thsp1900 <thsp1900@student.miun.se>
2026-03-09 18:58:06 +0100

    Add organizer event component

---
+++ src/app/features/organizer-event.component/organizer-event.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer-event.component',
+  imports: [],
+  templateUrl: './organizer-event.component.html',
+  styleUrl: './organizer-event.component.scss',
+})
+export class OrganizerEventComponent {
+
+}
