41599b480c61603a02606d90faa1986769a3f1e1
thsp1900 <thsp1900@student.miun.se>
2026-03-10 06:32:11 +0100

    Add stuff

--- src/app/features/organizer-event.component/organizer-event.component.ts
+++ src/app/features/organizer-event.component/organizer-event.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BackendService } from '../../services/backend.service';
+import { Event } from '../../models/event';
 
 @Component({
-  selector: 'app-organizer-event.component',
-  imports: [],
+  selector: 'app-organizer-event',
   templateUrl: './organizer-event.component.html',
-  styleUrl: './organizer-event.component.scss',
+  styleUrls: ['./organizer-event.component.scss']
 })
-export class OrganizerEventComponent {
+export class OrganizerEventComponent implements OnInit {
 
+  eventId!: string;
+  event?: Event;
+
+  constructor(
+    private route: ActivatedRoute,
+    private backendService: BackendService
+  ) {}
+
+  /**
+   * On component initialization, extract the event ID from the route parameters
+   * and load the event details from the backend.
+   */
+  async ngOnInit() {
+
+    this.eventId = this.route.snapshot.paramMap.get('eventId')!;
+    this.event = await this.backendService.getEventById(this.eventId);
+    console.log('Loaded event:', this.event);
+  }
 }
